343c212d03436c5c33db8114ed26356991dd0e28
alja1313 <olgaslahota@gmail.com>
2024-06-12 19:03:45 +0200

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -16,8 +16,12 @@ function generateTravelPlan(event) {
   let apiKey = "3t1efbo10d0b2a5b53045a2c4a8666fe";
   let prompt = `User instructions are: Generate one day travel itinerary in ${userInstructions.value}`;
   let context =
-    "You are experienced travel expert. Your mission is to generate 5 most popular places to visit. Answer should be provided in basic HTML. Make sure to follow user instructions.";
+    "You are experienced travel expert. Your mission is to generate 3 most popular places to visit. Answer should be provided in basic HTML. Make sure to follow user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let aiResponseElement = document.querySelector("#ai-response");
+  aiResponseElement.classList.remove("hidden");
+  aiResponseElement.innerHTML = `<div class="generating">⏳ Generating a travel plan in ${userInstructions.value}</div>`;
 
   console.log("Generating Travel Itinerary");
   console.log(`Prompt: ${prompt}`);
